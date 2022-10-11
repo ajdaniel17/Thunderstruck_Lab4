bc38c60f50bdb23b6b22e60b2916b21c4e5b99f1
ajdaniel17 <ajdaniel17@yahoo.com>
2022-10-10 19:57:15 -0500

    Started on the Music Driver

--- Music_Driver.py
+++ Music_Driver.py
@@ -1,1 +1,33 @@
-#All the fun goes here
+#import RPi.GPIO as GPIO
+from time import sleep
+import threading
+import numpy as np
+
+from Lookuptable import Note_Frequencies
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
+
+class Music_Driver (threading.Thread):
+    port = 0
+    BPM = 0
+
+    def __init__(self,port):
+        threading.Thread.__init__(self)
+        self.port = port
+        #NOTE: ALL VALUES IN ARRAY ARE ALL STRINGS, TYPE CONVERT WHEN NEEDED
+        self.Note_Frequencies = np.load('Note_Frequencies.npy')
+        # GPIO.setup(self.port,GPIO.OUT)
+
+    def setBPM(self,BPM):
+        self.BPM = BPM
+
+    def getBPM(self):
+        return self.BPM
+
+    
+
+Test = Music_Driver(2)
+
+
+
